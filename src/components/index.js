703e7ded7d03dc1dec848ecb7c0ce2ec1926d6d3
Akshat Gupta <akshatgtc@gmail.com>
2023-08-27 23:04:13 +0530

    header done

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Button from "./Button.jsx";
+import Nav from "./Nav.jsx/index.js";
+import ShoeCard from "./ShoeCard.jsx/index.js";
+import PopularProductCard from "./PopularProductCard.jsx/index.js";
+import ServiceCard from "./ServiceCard.jsx/index.js";
+import ReviewCard from "./ReviewCard.jsx/index.js";
+
+export {
+    Button,
+    Nav,
+    ShoeCard,
+    PopularProductCard,
+    ServiceCard,
+    ReviewCard,
+}
